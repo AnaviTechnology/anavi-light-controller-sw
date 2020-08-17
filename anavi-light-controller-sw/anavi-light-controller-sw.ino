332c5b31e60cbbad103cdf1a924467033091bb2b
Dejan Mircic <mircicd@student.ethz.ch>
2020-08-17 15:04:17 +0200

    more fine-grained temp change check

--- anavi-light-controller-sw/anavi-light-controller-sw.ino
+++ anavi-light-controller-sw/anavi-light-controller-sw.ino
@@ -1069,7 +1069,7 @@ void handleHTU21D()
 {
     // Check if temperature has changed
     const float tempTemperature = htu.readTemperature();
-    if (1 <= abs(tempTemperature - sensorTemperature))
+    if (0.1 <= fabs(tempTemperature - sensorTemperature))
     {
         // Print new temprature value
         sensorTemperature = tempTemperature;
